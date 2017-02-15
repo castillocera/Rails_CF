f280829aa2ff40243816fa3540122f779e8460ed
Brayan Murphy <BrayanMurphyC@gmail.com>
2017-02-14 22:48:19 -0500

    CALLBACK
    USO DE CALLBACKS EN EL MODELO Y CONTROLADOR DIFERENCIAS  USAR SELF

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,4 +1,13 @@
 class ArticlesController < ApplicationController
+#CALLBACK se tiene que registrar para que publique:, para que valide o permite hacer en todo exceptop en sohw e index para que la gente pueada lerr los articulos sin loguearse only: [:create,:new] con only las CALLBACK sera solo hazlo en crear y new si no se logueo
+#Sirve para ejecutar METODOS antes o despues que suceda acciones en el MODELO o CONTROLADOR
+#before_action existe en los controladores lo demas es en el modelo CREANDO OBJETOS http://www.guiasrails.es/active_record_callbacks.html
+#before_action :validate_user, except: [:show, :index] #Es CALLBACK CREAMOS UN OBJETO ABAJO, podemos ejecutar en el proceso de rails mientras ejecuta, no dejara comentar mientras no este logueado
+before_action :authenticate_user! #esta es un CALLBACK de DEVISE que trae es lo mismo que de arriba la funcion que no deja crear articulos a una eprsona que no esta logueada
+                                 #cuando una persona no esta logueada al momento de crear en create dira que no existe el metodo articles para current_user que esta en CREATE de este controlador
+
+#OPTIMIZAR CODIGO REPETIDO CON CALLBACK o refactor
+before_action :set_article, except: [:index ,:new, :create] #index,create y new no se repetian ese codigo que esta en el metodo set_article
 
 # get 'articles/index'
 #ACCION QUE TRAE LA LISTA DE LOS ARTICULOS
@@ -32,20 +41,20 @@ def create
 
  # GET /articles/:id articles#show
  #SHOW muestra recursos en especificooo, EN ESTE CASO EL ARTICULO ESPECIFICOoo POR SU ID
-  def show  #WHERE               #Find  busca registros por id
-      @article = Article.find(params[:id])
-
+  def show  #WHERE   #Find  busca registros por id
+      #@article = Article.find(params[:id]) #optimizamos con CALLBACK before_action
+      @article.update_visits_count #llamamos del modelo que se hizo CALLBACK para que sume las visitas segun se actualice la pagina
   end
 
   def edit
-      @articley = Article.find(params[:id]) #antes de encontrar la vista, encontrara el articulo a editar por su ID
+      #@articley = Article.find(params[:id]) #antes de encontrar la vista, encontrara el articulo a editar por su ID #optimizamos con CALLBACK before_action
   end
 
 #Si el objeto esta en la BD lo manda a UPDATE, al actualizar O editar
   def update
     #UPDATE
    #   @article.update_attributes({title: 'Nuevo titulo'})
-   @articley = Article.find(params[:id])
+   #@articley = Article.find(params[:id]) #optimizamos con CALLBACK before_action
    if @articley.update(article_params) #pasa los parametros fuertes o strong paramete, que es lo mismo que Article.update(title: params[:article][:title],body: params[:article][:body])
       redirect_to @articley            #solo permitira los parametros fuertes lo que se pide en el formulario el titulo y cuerpo
    else
@@ -54,9 +63,9 @@ def create
   end
 
  # DELETE /articles/:id      articles#destroy
-  def destroy
+  def destroy #refactor CALLBACK
      #DELETE FROM articles
-     @article = Article.find(params[:id]) #busca el articulo que desea eliminar el usuario mediante su ID
+    # @article = Article.find(params[:id]) #busca el articulo que desea eliminar el usuario mediante su ID #optimizamos con CALLBACK before_action
      @article.destroy #destruye el objeto de la BD
      redirect_to articles_path #redirecciona
   end
@@ -68,5 +77,15 @@ def create
   def article_params #el article esta en el nombre de los formularios  con un parametro que pide title y body
      params.require(:article).permit(:title,:body) #requiere los parametros que manda los campos del formulario solo permitira los que vengand de la peticion
   end                                              #sOLO ESTOS CAMPOS PERMITIRAN CREARSE CUANDO SE MANDA DESDE LA VISTA DEL FORMULARIO AL create
+
+#CALLBACK
+  def validate_user  #CALLBACK mandara a iniciar sesion a la persona que cree articulos sin estar logueado
+     redirect_to new_user_session_path, notice: "Necesitas iniciar sesión"
+  end
+
+ #optimizar los codigos repetidos refactor CALLBACK
+ def set_article
+    @article = Article.find(params[:id]) #optimizamos con CALLBACK before_action aca lo llamara para todos los metodos
+ end
 
 end
--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,6 +1,23 @@
 class Article < ApplicationRecord
    #VALIDACIONES, aca tambm es un hash pero ruby es flexible y permite ponerlo asi:
-   belongs_to :user #configuracion de tablas relaciones de uno a muchos, ponemos el modelo http://codehero.co/activerecord-asociaciones/ 
+   belongs_to :user #configuracion de tablas relaciones de uno a muchos, ponemos el modelo http://codehero.co/activerecord-asociaciones/
    validates :title, presence:true, uniqueness:true #requiere llenar el titulo
    validates :body, presence:true, length: { minimum: 5 } #requiere llenar y minimo con 5 letras
+
+   #CALLBACK-creando un OBJETO  -  Active Record http://www.guiasrails.es/active_record_callbacks.html
+   #Sirve para ejecutar METODOS antes o despues que suceda acciones en el MODELO o CONTROLADOR
+   #para que se sume las visitas en uno cuando visite el articulo
+   #before_create :set_visits_count #visits_count esta en el modelo article anteponer set es concenvion en los CALLBACK que haran refactor o meteran algun valor en el modelo
+   before_save :set_visits_count #guardara
+
+#metodo que llama el metodo SHOW en el controlador article
+   def update_visits_count    #le decimos que actualice el contador de visitas mas el valor del contador de visitas mas uno, este esta en el controlador SHOW que lo usara
+      #self.save if self.visits_count.nil?  #que guarde el modelo que ejecutara before_save, la cual ejecutara el metodo set_visits_count, cuando estaba sin valor el contador pero si ya tienen valor se lo quita
+      self.update(visits_count: self.visits_count + 1) #el metodo show cada ves que mande actualizara el contador mas 1
+   end
+
+   private
+   def set_visits_count
+      self.visits_count ||= 0 #si el valor de visits_count es nullo que le asigne cero ese valor, si no es nulo que lo deje como estaba
+   end
 end
